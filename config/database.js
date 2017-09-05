c4cc7bddd9c4d01ea95d0ab7a183eaf8d6c1845a
Sujeet Pandey <sujeetpaandey@gmail.com>
2017-09-05 17:28:20 +0530

    commit message

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-	url : 'mongodb://dev-sms:dev-sms@ds125774.mlab.com:25774/dev-sms'
+    //url : 'mongodb://dev-sms:dev-sms@ds125774.mlab.com:25774/dev-sms'
+    remoteUrl: 'mongodb://qauser:qauser@ds151702.mlab.com:51702/qa-blume'
 }
