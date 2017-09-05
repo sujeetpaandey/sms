9d7db40d353d63096004b227d9811a787123077c
Sujeet Pandey <sujeetpaandey@gmail.com>
2017-09-05 15:41:12 +0530

    commit message

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var bodyParser = require('body-parser'); // pull information from HTML POST (exp
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
 // configuration ===============================================================
-mongoose.connect(database.url); // connect to mongoDB database on modulus.io
+//mongoose.connect(database.url); // connect to mongoDB database on modulus.io
+mongoose.connect(database.url, { useMongoClient: true }); //MLab Connection String
 //mongoose.connect("127.0.0.1:27017/dev-sms-one"); // connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
@@ -19,6 +20,13 @@ app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-w
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
+// allow CORS ==================================================================
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // routes ======================================================================
 require('./app/routes.js')(app);
